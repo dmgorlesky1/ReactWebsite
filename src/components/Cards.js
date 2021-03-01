a7c6a02655cc54ffa99ef91ae1aedc9310c474b6
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2021-02-28 21:46:02 -0500

    Added wrapped cards so there is two cards on top and 3 on the bottom.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,12 +9,44 @@ function Cards(){
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem
+                            src={"images/img9.jpg"}
+                            text={"Explore the hidden waterfall deep inside the Amazon Jungle"}
+                            label={"Adventure"}
+                            path={"/services"}
+                        />
+                        <CardItem
+                            src={"images/img2.jpg"}
+                            text={"Travel through the islands of Bali in a private cruise"}
+                            label={"Luxury"}
+                            path={"/services"}
+                        />
+
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img4.jpg'
+                            text='Experience Football on Top of the Himilayan Mountains'
+                            label='Adventure'
+                            path='/products'
+                        />
+                        <CardItem
+                            src='images/img8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adrenaline'
+                            path='/sign-up'
+                        />
                     </ul>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Cards
